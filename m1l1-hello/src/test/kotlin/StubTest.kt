cc3d1671a7b3036a1a143cf1bbb533094c34d541
Кудрявцев Дмитрий Юрьевич <chrono.xcrosss@gmail.com>
2023-09-07 22:25:15 +0300

    hw1

---
+++ m1l1-hello/src/test/kotlin/StubTest.kt
@@ -0,0 +1,14 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class StubTest {
+
+    @Test
+    fun test(){
+        assertEquals(4, 2*2)
+    }
+    @Test
+    fun test2(){
+        assertEquals(4, 2+2)
+    }
+}
